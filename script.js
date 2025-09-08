ae3ee2cbe45fabec7b7ee8a253100b0652d79eec
jesuss-10 <jesus.esuarez@eaportal.org>
2025-09-08 11:08:59 -0300

    8-09-2025

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('demoBtn').addEventListener('click', function() {
+    document.getElementById('demoText').textContent = 'Você clicou no botão! Isso é JavaScript em ação 🚀';
+});
